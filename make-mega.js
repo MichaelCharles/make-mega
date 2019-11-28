3e73757a190fd83d57fb6aae98179b9f472c6348
Michael Aubrey <michael@akita-itlabo.com>
2019-11-29 00:53:24 +0900

    attempt to fix problem running from npx

--- make-mega.js
+++ make-mega.js
@@ -1,40 +1,10 @@
 #!/usr/bin/env node
-"use strict";
-const fs = require("fs"),
-  mb = 1000000,
-  size = process.argv[2] || 100;
+var program = require('commander')
+var makeMega = require('./')
+var package = require('./package.json')
 
-let content = "0";
+program
+  .version(package.version)
+  .usage('100')
 
-function main() {
-  if (size > 700) throw new Error("Sorry, I can only make files up to 700mb.");
-
-  console.log("Generating base string...");
-  while (content.length < size * mb) {
-    let additive = content;
-    if (additive.length > mb * 600) additive = additive.substring(0, 10 * mb);
-    if (additive.length > mb * 500) additive = additive.substring(0, 50 * mb);
-    content = `${content}${additive}`;
-  }
-
-  console.log(`Trimming to ${size}mb...`);
-  const offset = content.length - mb * size;
-  content = content.substring(0, content.length - offset);
-
-  console.log(`Attempting to write...`);
-  // eslint-disable-next-line node/prefer-promises/fs
-  fs.writeFile(
-    Math.floor(content.length / 1000 / 1000) + "-mb.txt",
-    content,
-    error => {
-      if (error) throw error;
-      console.log(`${size}mb file successfully created.`);
-    }
-  );
-}
-
-try {
-  main();
-} catch ({ message }) {
-  console.log(message);
-}
+makeMega()
